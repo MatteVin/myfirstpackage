9b03f923ffd7e87316af37c5dcbb607129ac5c48
matteo vindrola <matte.vindrola@gmail.com>
2020-03-13 00:22:04 -0700

    fix travis and get started with my_lm

---
+++ R/my_lm.R
@@ -0,0 +1,47 @@
+#' Fitting Linear Models
+#'
+#' This function is used to fit linear models. It can be used to carry out
+#' regressions.
+#'
+#' @param  formula a \code{formula} class object: a symbolic description of the
+#' model to be fitted.
+#' @param  data input data frame.
+#' @keywords t-test
+#'
+#' @return A list with elements: \code{test_stat}, the numeric test statistic;
+#' \code{df}, a numeric containing the degrees of freedom; \code{alternative}
+#' character containing the value of the parameter alternative; \code{p_val},
+#' a numeric p-value.
+#'
+#' @examples
+#' t_test(c(1, 5 ,4 , 5, 2, 6, 4), greater, 5)
+#' t_test(1:20, two.sided, 18)
+#'
+#' @export
+#Creates my_lm a function that takes in as parameters a formula and a dataset
+#returning a table containing the appropiate coefficients for the linear model.
+my_lm <- function(formula_lm, data_lm) {
+  #Extracts model matrix X.
+  X_lm <- model.matrix(formula_lm, data =  data_lm)
+  #Extract a model respopnse Y.
+  Y_lm <- model.response(model.frame(data_lm))
+  #Estimates linear regression coefficients.
+  beta_lm <- solve(t(X_lm) %*% X_lm) %*%  t(X_lm) %*% Y_lm
+  #Estimates the degrees of freedom.
+  df_lm <- nrow(data_lm) - nrow(beta_lm)
+  #Estimates the variance.
+  var_lm <- sum(((Y_lm - X_lm %*% beta_lm )^2)) / df_lm
+  #Estimates the standard error.
+  suppressWarnings(std_error_lm <- diag(sqrt(var_lm * solve(t(X_lm) %*% X_lm))))
+  #Estimates the t value.
+  t_value_lm <- beta_lm / std_error_lm
+  #Estimates the p value of the t test.
+  p_of_t <- pt(abs(t_value_lm), df_lm, lower.tail = FALSE)*2
+  #Combines coefficients, standard errors, t values and p values of the t test,
+  #all in one matrix
+  final_matrix <- cbind(beta_lm, std_error_lm, t_value_lm, p_of_t)
+  #appropiatly names the culums of the matrix
+  colnames(final_matrix) <-  c("Estimate", "Std. Error", "t value", "Pr(>|t|)")
+  #returns the final matrix as a table
+  return(as.table(final_matrix))
+}
