67bb060cc6ecc550063e9fd699f52bda3e5fab4c
matteo vindrola <matte.vindrola@gmail.com>
2020-03-14 11:46:12 -0700

    finish my_lm and fix travis

--- R/my_lm.R
+++ R/my_lm.R
@@ -3,23 +3,33 @@
 #' This function is used to fit linear models. It can be used to carry out
 #' regressions.
 #'
-#' @param formula_lm formula
-#' @param data_lm dtatset
+#' @param formula a \code{formula} class object: a symbolic description of the
+#' model to be fitted.
+#' @param data input data frame.
+#'
+#' @return A table containing all numerics, with one row for each coefficient
+#' including the intercept and colums for the \code{Estimate}, the
+#' \code{Std. Error}, the \code{t value}, and \code{Pr(>|t|)} (the p-value for
+#' the t-test).
+#'
+#' @examples
+#' my_lm(c(1, 5 ,4 , 5, 2, 6, 4), "greater", 5)
+#' my_lm(1:20, "two.sided", 18)
 #'
 #' @import stats
 #'
 #' @export
 #Creates my_lm a function that takes in as parameters a formula and a dataset
 #returning a table containing the appropiate coefficients for the linear model.
-my_lm <- function(formula_lm, data_lm) {
+my_lm <- function(formula, data) {
   #Extracts model matrix X.
-  X_lm <- model.matrix(formula_lm, data =  data_lm)
+  X_lm <- model.matrix(formula, data)
   #Extract a model respopnse Y.
-  Y_lm <- model.response(model.frame(data_lm))
+  Y_lm <- model.response(model.frame(data))
   #Estimates linear regression coefficients.
   beta_lm <- solve(t(X_lm) %*% X_lm) %*%  t(X_lm) %*% Y_lm
   #Estimates the degrees of freedom.
-  df_lm <- nrow(data_lm) - nrow(beta_lm)
+  df_lm <- nrow(data) - nrow(beta_lm)
   #Estimates the variance.
   var_lm <- sum(((Y_lm - X_lm %*% beta_lm )^2)) / df_lm
   #Estimates the standard error.
