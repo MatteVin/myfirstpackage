dbc3ce1c7d9757e6e6a8db9bdc08801719185569
matteo vindrola <matte.vindrola@gmail.com>
2020-03-14 14:13:18 -0700

    Every package should work now

---
+++ R/my_gapminder.R
@@ -0,0 +1,21 @@
+#' Edgard Anderson's Iris Data
+#'
+#' Excerpt of the Gapminder data on life expectancy, GDP per capita,
+#' and population by country.
+#'
+#' @format a data frame with 1704 observations and 6 variables:
+#' \describe{
+#' \item{country}{factor with 142 levels, each one being a country}
+#' \item{continent}{factor with 5 levels}
+#' \item{year}{ranges from 1952 to 2007 in increments of 5 years}
+#' \item{lifeExp}{life expectancy at birth, in years}
+#' \item{pop}{population}
+#' \item{gdpPercap}{Gross Domestic Product per capita (US$, inflation-adjusted)}
+#'
+#' }
+#'
+#' @source
+#' \url{http://www.gapminder.org/data/}. Gapminder is an independent Sweedish
+#' foundation which claims to "fight devastating misconceptions about global
+#' development".
+"my_gapminder"
--- R/my_rf_cv.R
+++ R/my_rf_cv.R
@@ -10,15 +10,15 @@
 #'
 #' @keywords
 #'
-#' @import randomForest dplyr
+#' @import randomForest dplyr my_gapminder
 #'
 #' @export
 #My Random Forest Cross-Validation function
 my_rf_cv <- function(k){
   # Split data in k parts, randomly
-  folds <- sample(rep(1:k, length = nrow(iris)))
+  folds <- sample(rep(1:k, length = nrow(my_gapminder)))
   # Use data from before, leaving out species as we won't need it
-  data <- data.frame(iris[,-5], folds)
+  data <- data.frame(my_gapminder[,6], my_gapminder[,5], folds)
   #List for string cv errors on each iteration
   cv_err_list <- rep(NA, k)
   #loops trough the groups
@@ -28,16 +28,15 @@ my_rf_cv <- function(k){
     #set the testing set
     data_test <- data %>% filter(folds == i)  %>% select(-folds)
     #crates a random forest model on the training data
-    model_k_i <-
-      randomForest(Sepal.Length ~ Sepal.Width + Petal.Length + Petal.Width,
-                   data = data_train,
-                   ntree = 100
-      )
+    model_k_i <- randomForest(lifeExp ~ gdpPercap,
+                              data = data_train,
+                              ntree = 100
+                              )
     #calculates predictions on the test using the random forest model
-    predictions_k_i <- predict(model_k_i, data_test[, -1])
+    predictions_k_i <- predict(model_k_i, data_test["lifeExp"])
     #calculates the mean square error petween the predicted and true sepal.length
-    cv_err_list[i] <- sum((predictions_k_i - data_test[, 1])^2) /
-      (length(data_test[, 1]))
+    cv_err_list[i] <- sum((predictions_k_i - data_test["gdpPercap"])^2) /
+      (length(data_test[]))
   }
   #calculates and returns the mean cross validation MSE
   cv_err <- mean(cv_err_list)
