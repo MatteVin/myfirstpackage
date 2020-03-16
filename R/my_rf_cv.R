f2ede78c820689cd5e849755d22888390cac9e37
matteo vindrola <matte.vindrola@gmail.com>
2020-03-15 23:11:43 -0700

    Import fixed?

--- R/my_rf_cv.R
+++ R/my_rf_cv.R
@@ -1,7 +1,7 @@
 #' Random Forest Cross-Validation
 #'
-#' @import class magrittr randomForest
-#'
+#' @import class magrittr randomForest dplyr
+#' @importFrom magrittr %>%
 #' @export
 #My Random Forest Cross-Validation function
 my_rf_cv <- function(k){
